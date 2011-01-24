37340bd68d55b88e0fcecdbfbe24e180239135e9
Fábio Miranda Costa <fabio.costa@corp.globo.com>
2011-01-24 14:24:01 -0200

    fix, not using custom reporters anymore

--- runner.js
+++ runner.js
@@ -59,9 +59,6 @@ for(var key in jasmine)
 
 require('./Helpers/JSSpecToJasmine');
 
-var reporter = require('reporters/' + (options.reporter || 'console')).Reporter;
-reporter.done = function(runner, log){
-  process.exit(runner.results().failedCount);
-};
-
-jasmine.runSpecs(specs, reporter);
+jasmine.runSpecs(specs, function(runner, log){
+	process.exit(runner.results().failedCount);
+}, true, true);
